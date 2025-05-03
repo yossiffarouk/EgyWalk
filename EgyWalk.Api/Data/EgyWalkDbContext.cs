2f7fb0b51f1ad07fb1298e86e896764b490469ec
yossiffarouk <yossif155farouk@gmail.com>
2025-05-03 20:48:31 +0300

    Added DbContext with 3 db tables

---
+++ EgyWalk.Api/Data/EgyWalkDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EgyWalk.Api.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EgyWalk.Api.Data
+{
+    public class EgyWalkDbContext :DbContext
+    {
+        public EgyWalkDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
---
+++ EgyWalk.Api/Models/Domain/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace EgyWalk.Api.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
